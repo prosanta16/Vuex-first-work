f2d761ccf636b4b586d0b1487266f0a87b89d9e1
Prosanta Paul <prosantapaulbsmrstu@gmail.com>
2021-01-17 21:24:51 +0600

    initail

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <compBookListOne :book="books"></compBookListOne>
+        <compBookListTwo :book="books"></compBookListTwo>
+    </div>
+</template>
+
+<script>
+import compBookListOne from "./components/BookListOne"
+import compBookListTwo from "./components/BookListTwo"
+export default {
+    data(){
+        return{
+            books: [
+              {name: "Harry Potter", price: 100},
+              {name: "Lord of The Rings", price: 150},
+              {name: "Night Circus", price: 90},
+              {name: "Lost World", price: 200}
+            ]
+        }
+    },
+    components:{
+      compBookListOne,
+      compBookListTwo
+    }
+}
+</script>
