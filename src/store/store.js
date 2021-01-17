2fe800e85249745581528dd91f6fcd7cf5d05289
Prosanta Paul <prosantapaulbsmrstu@gmail.com>
2021-01-17 21:45:51 +0600

    forget to using state

--- src/store/store.js
+++ src/store/store.js
@@ -4,11 +4,13 @@ import Vuex from 'vuex'
 Vue.use(Vuex);
 
 export const store=new Vuex.Store({
-    books: [
-        {name: "Harry Potter", price: 100},
-        {name: "Lord of The Rings", price: 150},
-        {name: "Night Circus", price: 90},
-        {name: "Lost World", price: 200}
-      ]
+    state:{ 
+        books: [
+            {name: "Harry Potter", price: 100},
+            {name: "Lord of The Rings", price: 150},
+            {name: "Night Circus", price: 90},
+            {name: "Lost World", price: 200}
+      ]}
+   
 
 })
